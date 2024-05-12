19f8af96befdeeb97a1b921dbe6e2be53fed1c33
Vincent Thomas <77443389+vincent-thomas@users.noreply.github.com>
2024-05-12 03:31:35 +0200

    nice nvim

--- home.nix
+++ home.nix
@@ -63,7 +63,14 @@
     enable = true;
     plugins = with pkgs.vimPlugins; [
       # UI
-      lightline-vim
+      {
+        plugin = lightline-vim;
+        config = ''
+          let g:lightline = {
+            \ 'colorscheme': 'one',
+          \ }
+        '';
+      }
       {
         plugin = onedarkpro-nvim;
         config = "colorscheme onedark";
@@ -71,12 +78,21 @@
  
       # Nice saker
       # Fix comments TODO: .nix filer
-      commentary
+      {
+        plugin = commentary;
+        config = "nnoremap cl <cmd>Commentary<cr>";
+      }
+      {
+        plugin = todo-comments-nvim;
+        config = "nnoremap ;t <cmd>TodoTelescope<cr>";
+      }
 
+      gitsigns-nvim
       # LSP
       nvim-lspconfig
 
       # Functional
+      auto-pairs
       {
         plugin = telescope-nvim;
         config = ''
@@ -85,16 +101,75 @@
         '';
       }
       editorconfig-nvim
+      conform-nvim
+      copilot-cmp
+      copilot-lua
+      cmp-path
 
       # Autocomplete
       nvim-cmp
-
       cmp-nvim-lsp
+      luasnip
 
       # Syntax Highlighting
-      (pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [ p.rust p.typescript p.javascript ]))
+      (pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [ p.rust p.typescript p.javascript p.yaml p.toml p.json ]))
    ];
     extraLuaConfig = ''
+
+      require('todo-comments').setup {
+        keywords = {
+          FIX = {
+            icon = " ", -- icon used for the sign, and in search results
+            color = "error", -- can be a hex color, or a named color (see below)
+            alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+            -- signs = false, -- configure signs for some keywords individually
+          },
+          TODO = { icon = " ", color = "info" },
+          HACK = { icon = " ", color = "warning" },
+        },
+        gui_style = {
+          fg = "NONE",
+          bg = "BOLD",
+        },
+        colors = {
+          error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+          warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+          info = { "DiagnosticInfo", "#2563EB" },
+          hint = { "DiagnosticHint", "#10B981" },
+          default = { "Identifier", "#7C3AED" },
+          test = { "Identifier", "#FF00FF" }
+        },
+        search = {
+          command = "rg",
+          args = {
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+          },
+        },
+      }
+
+      require("conform").setup({
+        format_on_save = {
+          -- These options will be passed to conform.format()
+          timeout_ms = 500,
+          lsp_fallback = true,
+        },
+      })
+
+      require('gitsigns').setup {
+        current_line_blame = true
+      }
+
+      require("copilot_cmp").setup()
+      require('copilot').setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false }
+      })
+
+
       vim.opt.tabstop = 2
       vim.opt.softtabstop = 2
       vim.opt.shiftwidth = 2
@@ -121,28 +196,47 @@
       local cmp = require'cmp'
 
       cmp.setup {
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end
+        },
         mapping = cmp.mapping.preset.insert({
           ['<C-Space>'] = cmp.mapping.complete(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<Tab>'] = cmp.mapping.confirm({ select = true }),
         }),
-        sources = {
-          { name = 'nvim_lsp' }
-        },
+
+        sources = cmp.config.sources({
+          { name = 'copilot' },
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+          { name = 'path' },
+        }, { name = 'buffer' }),
       }
+
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-      require('lspconfig')
       require('lspconfig').rust_analyzer.setup {
         capabilities = capabilities,
-        on_attach = (function(client, bufnr)
-          local opts = {buffer = bufnr, remap = false}
-
-          vim.keymap.set("n","rr", vim.lsp.buf.rename, opts)
-        end),
         settings = {
           ['rust-analyzer'] = {},
         },
       }
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        desc = 'LSP Actions',
+        callback = function()
+          local bufmap = function(mode, lhs, rhs)
+            local opts = {buffer = bufnr, remap = false}
+            vim.keymap.set(mode, lhs, rhs, opts)
+          end
+
+          bufmap("n","gr", vim.lsp.buf.rename)
+          bufmap("n","gd", vim.lsp.buf.definition)
+          bufmap("n","gD", vim.lsp.buf.declaration)
+          bufmap("n","gh", vim.lsp.buf.hover)
+        end
+      })
     '';
     extraConfig = ''
       filetype plugin indent on
@@ -153,7 +247,6 @@
 
   programs.zellij = {
     enable = true;
-    enableZshIntegration = true;
   };
 
   home.packages = with pkgs; [
@@ -162,6 +255,7 @@
     ripgrep
     bacon
     rustup
+    nodejs_20
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
